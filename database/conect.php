1aa4377ff7d25b6e4ac325dc66fa62345b864110
Leonardo Zambão <leonardozambao@live.com>
2020-05-25 20:33:31 -0300

    inclusão de assets

--- database/conect.php
+++ database/conect.php
@@ -1,11 +1,34 @@
 <?php
-$conect = mysqli_connect(
-    "localhost", //host
-    "root", //user
-    "",  //password
-    "db_projetoarquitetura" //nome do banco
-);
 
-if(mysqli_connect_errno()){
-    die("Conexão falhou: ".mysqli_connect_errno());
+class Database {
+	private $connection;
+	private static $instance; 
+	private $host = "localhost";
+	private $username = "root";
+	private $password = ""; //localhost sem senha
+	private $database = "db_projetoarquitetura";
+
+	public static function getInstance() {
+		if(!self::$instance) { 
+			self::$instance = new self();
+		}
+		return self::$instance;
+	}
+
+	private function __construct() {
+		$this->connection = new mysqli($this->host, $this->username, 
+			$this->password, $this->database);
+	
+		if(mysqli_connect_error()) {
+			trigger_error("Algo de errado não está certo: " . mysql_connect_error(),
+				 E_USER_ERROR);
+		}
+	}
+
+	// evita duplicação de conexão
+	private function __clone() { }
+
+	public function getConnection() {
+		return $this->connection;
+	}
 }
